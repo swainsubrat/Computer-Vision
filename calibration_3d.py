d97f4613a96ba21b7d19acbfbfd32fec8812e4d3
swainsubrat <mailofswainsubrat@gmail.com>
2022-02-04 16:16:08 +0530

    added complete calibration code

--- calibration_3d.py
+++ calibration_3d.py
@@ -1,6 +1,5 @@
-import math
 import numpy as np
-from numpy import append, linalg as LA
+from numpy import linalg as LA
 
 ###### CONSTANTS ######
 # XY -> XZ -> YZ
@@ -17,24 +16,34 @@ brown     = (447, 1033)
 # pink      = (406, 1022)
 l_green   = (448, 986)
 
-V = [[486,920],[526,927],[487,869],[526,875],[448, 1007],[366,1008],[447,1033],[406,1022],[448,986]]
+V = [[486,920],[526,927],[487,869],[526,875],[448,1007],[366,1008],[447,1033],[406,1022],[448,986]]
 
 xw = []     #xw is X-axis coordinates array of image all points
 yw = []     #yw is y-axis coordinates array of image all points
 zw = []     #zw is z-axis coordinates array of image all points
 u  = []     #u is u axis coordinates of all image points
 v  = []     #v is v axis coordinates of all image points
-image2d_vectors = [[486,920], [526,927], [526,875], [448, 1007], [447,1033], [448,986]]     #2d image coordinates [u,v] vector of all points
-realw_vectors = [[1, 1, 0], [2, 1, 0], [2, 2, 0], [2, 0, 2], [3, 0, 3], [1, 0, 1]]          #3d real world coordinates [x,y,z] vector of all points
-image3d_vectors = [[486,920,1], [526,927,1], [526,875,1], [448, 1007,1], [447,1033,1], [448,986,1]]    #3d image coordinates [u,v,1] vector
-real4d_vectors = [[1,1,0,1],[2,1,0,1], [2,2,0,1],[2,0,2,1], [3,0,3,1],[1,0,1,1]]                       #4d image coordinates [x,y,z,1] vector
-# Now creating a vector of points in X axis, y axis, z axis points separately 
+
+image2d_vectors = [[486,920], [526,927], [526,875], [448, 1007], [447,1033], [448,986]]     # 2d image coordinates [u,v] vector of all points
+realw_vectors   = [[1, 1, 0], [2, 1, 0], [2, 2, 0], [2, 0, 2], [3, 0, 3], [1, 0, 1]]        # 3d real world coordinates [x,y,z] vector of all points
+
+# Normalize the vectors
+
+
+image3d_vectors = [v + [1] for v in image2d_vectors] # 3d image coordinates [u,v,1] vector
+real4d_vectors  = [v + [1] for v in realw_vectors]   # 4d image coordinates [x,y,z,1] vector
+
+# image3d_vectors = [[486,920,1], [526,927,1], [526,875,1], [448, 1007,1], [447,1033,1], [448,986,1]]    #3d image coordinates [u,v,1] vector
+# real4d_vectors = [[1,1,0,1],[2,1,0,1], [2,2,0,1],[2,0,2,1], [3,0,3,1],[1,0,1,1]]                       #4d image coordinates [x,y,z,1] vector
+
+# Now creating a vector of points in x axis, y axis, z axis points separately 
 for i in range(len(real4d_vectors)):
-    xw.append(real4d_vectors[i][0])         #all x axis components
-    yw.append(real4d_vectors[i][1])         #all y axis components
-    zw.append(real4d_vectors[i][2])         #all z axis components
-print(xw, yw, zw)
-# convert point vectors from sleeping vector (Rows) to standing vectors (Column) of real world
+    xw.append(real4d_vectors[i][0])         # all x axis components
+    yw.append(real4d_vectors[i][1])         # all y axis components
+    zw.append(real4d_vectors[i][2])         # all z axis components
+# print(xw, yw, zw)
+
+# Convert point vectors from sleeping vector (Rows) to standing vectors (Column) of real world
 xwt = np.array(xw).transpose()          
 ywt = np.array(yw).transpose()
 zwt = np.array(zw).transpose()
@@ -42,9 +51,9 @@ zwt = np.array(zw).transpose()
 for i in range(len(image3d_vectors)):
     u.append(image3d_vectors[i][1])
     v.append(image3d_vectors[i][1])
+# print(u,v)
 
-print(u,v)
-# convert point vectors from sleeping vector (Rows) to standing vectors (Column) of image points
+# Convert point vectors from sleeping vector (Rows) to standing vectors (Column) of image points
 ut = np.array(u).transpose()
 vt = np.array(v).transpose()
 Au = [[]]
@@ -59,24 +68,19 @@ for i in range(6):
     A.append(Au[i])
     A.append(Av[i])
 A = np.array(A)
-print(A)
-#Now A matrix of 12*12 is generated
-#target_matrix = np.dot(A, A.T)
-#values, vectors = LA.eig(target_matrix)
+# print(A)
 
-#print(values, vectors)
-
-A = np.diag((1, 2, 3, 4))
-
-target_matrix = np.dot(A, A.T)
+# Find the eigen vector corresponding to smallest eigen value
+target_matrix   = np.dot(A, A.T)
 values, vectors = LA.eig(target_matrix)
+smallest = vectors[np.argmin(values)].reshape(3, 4)
 
-print(values)
-print(vectors)
-
-smallest = vectors[0]
-reshape_size = (int(math.sqrt(smallest.shape[0])), int(math.sqrt(smallest.shape[0])))
-smallest = smallest.reshape(reshape_size)
-print(smallest)
-R, K = LA.qr(smallest)
-print(R, K)
+# QR Factorization
+qr = smallest[:,:-1]
+R, K = LA.qr(qr)
+trans = np.dot(LA.inv(K), smallest[:,-1])
+print(f"Rotational Matrix is: \n{R}")
+print(f"K Matrix is: \n{K}")
+print(f"Translational Vector is{trans}")
+print(f"Focal length x and Focal length y: {K[0][0]}, {K[1][1]}")
+print(f"Ox and Oy: {K[0][-1]}, {K[1][-1]}")
